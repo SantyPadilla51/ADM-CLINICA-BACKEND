8531d4aaa321492074ebd43461b9a3a3c4e75d18
santiago <padillasantiago51@gmail.com>
2025-04-18 14:00:18 -0300

    edit cors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import { connectDB } from "./config/db.js"
-import router from "./routes/PacientesRouter.js"
-import routerDoc from "./routes/DoctorRouter.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import router from "./routes/PacientesRouter.js";
+import routerDoc from "./routes/DoctorRouter.js";
 
+dotenv.config();
+const app = express();
 
-dotenv.config()
-const app = express()
-
-app.use(cors({
-    origin: ["https://admin-pacientes-ecru.vercel.app", "http://localhost:5173"],
+app.use(
+  cors({
+    origin: [
+      "https://adm-clinica-frontend.vercel.app",
+      "http://localhost:5173",
+    ],
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-
-app.options('*', cors());
+app.use(express.json());
 
 const iniciarServer = async () => {
-    await connectDB();
+  await connectDB();
 
-    app.use(router);
-    app.use(routerDoc);
+  app.use(router);
+  app.use(routerDoc);
 
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => {
-        console.log(`Conectado al puerto: ${PORT}`);
-    });
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Conectado al puerto: ${PORT}`);
+  });
 };
 
 iniciarServer();
