de9264cef2b8bef4e23768753de7348cc3c8f5cd
santiago <padillasantiago51@gmail.com>
2025-07-21 19:55:52 -0300

    fix db.js and Models

--- src/models/DoctorModels.js
+++ src/models/DoctorModels.js
@@ -1,63 +1,66 @@
-import { Sequelize } from 'sequelize';
-import bcrypt from 'bcrypt';
-import db from "../config/db.js";
-import generarID from '../helpers/generarID.js';
+import { Sequelize } from "sequelize";
+import bcrypt from "bcrypt";
+import { db } from "../config/db.js";
+import generarID from "../helpers/generarID.js";
 
-const Doctor = db.define('doctores', {
+const Doctor = db.define(
+  "doctores",
+  {
     nombre: {
-        type: Sequelize.TEXT,
-        allowNull: false
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     apellido: {
-        type: Sequelize.TEXT,
-        allowNull: false
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     email: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-        unique: true,
+      type: Sequelize.TEXT,
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type: Sequelize.TEXT,
-        allowNull: false
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     dni: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        unique: true,
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: true,
     },
-    telefono:{
-        type: Sequelize.TEXT,
-        allowNull: true,
+    telefono: {
+      type: Sequelize.TEXT,
+      allowNull: true,
     },
-    especialidad:{
-        type: Sequelize.TEXT,
-        allowNull: true,
+    especialidad: {
+      type: Sequelize.TEXT,
+      allowNull: true,
     },
     token: {
-        type: Sequelize.Sequelize.TEXT,
-        defaultValue: generarID(),
+      type: Sequelize.Sequelize.TEXT,
+      defaultValue: generarID(),
     },
-    confirmado:{
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
+    confirmado: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
     },
-
-}, {
+  },
+  {
     hooks: {
-        beforeCreate: async (doctor) => {
-            if (doctor.password) {
-                const salt = await bcrypt.genSalt(10);
-                doctor.password = await bcrypt.hash(doctor.password, salt);
-            }
-        },
-        beforeUpdate: async (doctor) => {
-            if (doctor.changed('password')) {
-                const salt = await bcrypt.genSalt(10);
-                doctor.password = await bcrypt.hash(doctor.password, salt);
-            }
+      beforeCreate: async (doctor) => {
+        if (doctor.password) {
+          const salt = await bcrypt.genSalt(10);
+          doctor.password = await bcrypt.hash(doctor.password, salt);
         }
-    }
-});
+      },
+      beforeUpdate: async (doctor) => {
+        if (doctor.changed("password")) {
+          const salt = await bcrypt.genSalt(10);
+          doctor.password = await bcrypt.hash(doctor.password, salt);
+        }
+      },
+    },
+  }
+);
 
 export default Doctor;
--- src/models/HistorialModel.js
+++ src/models/HistorialModel.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import db from "../config/db.js";
+import { db } from "../config/db.js";
 
 const Historial = db.define("historiales", {
   id: {
